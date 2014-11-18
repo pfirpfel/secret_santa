c806e734ee9b1097634cb48992c1e9e19639976f
Michael Kuenzli <michaelkuenzli@gmail.com>
2014-11-18 10:30:15 +0100

    when really sending, silently write to log file instead

--- santa_logger.rb
+++ santa_logger.rb
@@ -2,8 +2,20 @@ class SantaLogger
   
   attr_accessor :sending
 
+  @@logname = 'santa.log'
+
+  def initialize
+    File.delete(@@logname)
+  end
+
   def log(message)
-    puts "#{message}" unless REALLY_SENDING # Sshh! It's a secret!
+    if REALLY_SENDING # silently write to log file (in case mail sending fails)
+      File.open(@@logname, 'a') do |f|
+        f.puts "#{message}"
+      end
+    else
+      puts "#{message}"
+    end
   end
 
 end
